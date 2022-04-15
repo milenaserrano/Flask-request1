b9ee62e8d68a5afef7b133ab7942fc36a2c44cf7
Milena <68025219+milenaserrano@users.noreply.github.com>
2022-04-15 20:13:41 +0000

    falta o bd

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
+
 from flask import Flask, render_template, request, redirect
 app = Flask('app')
 
-contacts = [
-  
-  { 'name': 'João da Silva' },
-  { 'name': 'Maria Souza'}
-]
+contacts = []
 
 @app.route('/')
 def index():
-  return render_template('index.html', contacts=contacts)
-
-
+  return render_template(
+    'index.html',
+    contacts=contacts
+  )
 
 @app.route('/create', methods=['POST'])
 def create():
   name = request.form.get('name')
-  contacts.append({'name': name})
+  email = request.form.get('email')
+  phone = request.form.get('phone')
+  contacts.append({
+    'name': name,
+    'email': email,
+    'phone': phone,
+  })
   return redirect('/')
 
+@app.route('/delete/<int:index>')
+def delete(index):
+  contacts.pop(index)
+  return redirect('/')
 
+@app.route('/update/<int:index>', methods=['POST'])
+def update(index):
+  name = request.form.get('name')
+  email = request.form.get('email')  
+  phone = request.form.get('phone')
+  contacts[index]['name'] = name
+  contacts[index]['email'] = email
+  contacts[index]['phone'] = phone
+  return redirect('/')
 
+# IMPORTANTE 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
